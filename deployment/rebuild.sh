5722a3e65f810e88cc8773b349324c42b091f892
mzoworka <mzoworka@gmail.com>
2025-09-24 23:44:58 +0200

    fix logging

--- deployment/rebuild.sh
+++ deployment/rebuild.sh
@@ -8,6 +8,6 @@ make run-dev
 cd $REPOSITORY_PATH/frontend
 npm install
 npm run build
-rm -rf $WEB_INSTALL_PATH
+rm -rf $WEB_INSTALL_PATH/*
 mkdir -p $WEB_INSTALL_PATH
 cp -r $REPOSITORY_PATH/frontend/dist/* $WEB_INSTALL_PATH
--- deployment/src/deployment.py
+++ deployment/src/deployment.py
@@ -1,4 +1,5 @@
 import datetime
+import subprocess
 import os
 import hmac
 import hashlib
@@ -34,6 +35,12 @@ def verify_signature(payload, signature):
     mac = hmac.new(GITHUB_WEBHOOK_SECRET.encode(), msg=payload, digestmod=hashlib.sha256)
     return hmac.compare_digest(mac.hexdigest(), signature)
 
+def rebuild():
+    timestamp = datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+    logs_path = os.path.join(LOGS_PATH, f"{timestamp}.log")
+    env = os.environ.copy()
+    p = subprocess.Popen([f"exec ./rebuild.sh 2>&1 | tee {logs_path}"], shell=True, preexec_fn=os.setsid, env=env)
+
 @app.route("/deploy/webhook", methods=["POST"])
 def webhook():
     # GitHub sends the signature header as 'X-Hub-Signature-256'
@@ -42,22 +49,15 @@ def webhook():
     if not verify_signature(payload, signature):
         abort(403, "Invalid signature.")
     event = request.headers.get("X-GitHub-Event", "")
-    if event != "deployment":
+    if event != "push":
         return "Ignored", 200
 
     # Optionally, validate action in payload JSON
     data = request.json
     # You could check more about deployment here, e.g., environment, branch, etc.
 
-    timestamp = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
-    logs_path = os.path.join(LOGS_PATH, f"{timestamp}.log")
-
-    # Execute the rebuild.sh script (ensure it is secure)
-    exit_code = os.system(f"bash ./rebuild.sh > {logs_path}")
-    if exit_code != 0:
-        return f"rebuild.sh failed with exit code {exit_code}", 500
+    rebuild()
     return "Deployment handled and rebuild.sh executed.", 200
 
 if __name__ == "__main__":
-    # For production, use a WSGI server instead!
-    app.run(host="0.0.0.0", port=8001)
+    rebuild()
